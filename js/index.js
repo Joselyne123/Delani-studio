694c7fb804547e3570f318eecac5af8290f866bb
joseingabire123@gmail.com <joseingabire123@gmail.com>
2020-10-04 18:53:56 +0200

    add jquery and document display images

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#productimg").click(function() {
+        $("#producttxt").show();
+    });
+    $("#producttxt").click(function(){
+        $("#producttxt").hide();
+    });
+    
+  });
+  $(document).ready(function() {
+    $("#devimg").click(function() {
+        $("#devtxt").show();
+    });
+    $("#devtxt").click(function(){
+        $("#devtxt").hide();
+    });
+    
+  });
+  $(document).ready(function() {
+    $("#designimg").click(function() {
+        $("#designtxt").show();
+    });
+    $("#designtxt").click(function(){
+        $("#designtxt").hide();
+    });
+    
+  });
