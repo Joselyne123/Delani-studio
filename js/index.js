ec20c89713eec384165e8295b357553e46b5d7b2
joseingabire123@gmail.com <joseingabire123@gmail.com>
2020-10-04 18:55:19 +0200

    add jquery function

--- js/index.js
+++ js/index.js
@@ -25,3 +25,49 @@ $(document).ready(function() {
     });
     
   });
+  $(document).ready(function () {
+    $('.Description').mouseover(function () {
+         $('.portimg').show();
+         $('.porttext').show();
+    })
+
+    $('.Description').mouseout(function () {
+         $('.portimg').hide();
+         $('.porttext').hide();
+    })
+});
+$(document).ready(function () {
+  $('.jojo').mouseover(function () {
+       $('.fix').show();
+       $('.nana').show();
+  })
+
+  $('.jojo').mouseout(function () {
+       $('.fix').hide();
+       $('.nana').hide();
+  })
+});
+    
+$(document).ready(function () {
+  $('.dada').mouseover(function () {
+       $('.d').show();
+       $('.ni').show();
+  })
+
+  $('.dada').mouseout(function () {
+       $('.d').hide();
+       $('.ni').hide();
+  })
+});
+    
+$(document).ready(function () {
+  $('.dudu').mouseover(function () {
+       $('.k').show();
+       $('.ki').show();
+  })
+
+  $('.dudu').mouseout(function () {
+       $('.k').hide();
+       $('.ki').hide();
+  })
+});
